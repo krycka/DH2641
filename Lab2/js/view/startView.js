afcbdb2ac0b9e5a22930c360dc31e60ab25bf5ff
krycka <rodoo@kth.se>
2016-02-17 14:49:24 +0100

    Folder rename.

---
+++ Lab2/js/view/startView.js
@@ -0,0 +1,13 @@
+//ExampleView Object constructor
+var startView = function (container) {
+	//alert("startView");
+	
+	// Get all the relevant elements of the view (ones that show data
+  	// and/or ones that responed to interaction)
+	//this.numberOfGuests = container.find("#numberOfGuests");
+	//this.plusButton = container.find("#plusGuest");
+	//this.minusButton = container.find("#minusGuest");
+	
+	//this.numberOfGuests.html("Hello World");
+	
+}
